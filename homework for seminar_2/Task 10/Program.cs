a78a1be87e9aff2d86cfc4119307bf3f765375f7
ales <a.i11@bk.ru>
2022-11-28 02:20:59 +0300

    Changes in Task 10

--- homework for seminar_2/Task 10/Program.cs
+++ homework for seminar_2/Task 10/Program.cs
@@ -1,2 +1,5 @@
-﻿// 
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine((a / 10) % 10);
