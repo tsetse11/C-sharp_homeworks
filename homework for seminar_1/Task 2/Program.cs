12c914c332b43409d1e524f3bede3dfb51178bdd
ales <a.i11@bk.ru>
2022-11-27 03:20:56 +0300

    Added gitignore

---
+++ homework for seminar_1/Task 2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Clear();
+Console.Write("Введите 1-е число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-е число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a <= b)
+    Console.Write($"{b} ");
+else
+    Console.Write($"{a} ");
---
+++ homework for seminar_1/Task 4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Clear();
+Console.Write("Введите 1-е число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-е число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-е число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max)
+    max = b;
+if (c > max)
+    max = c;
+Console.WriteLine($"max = {max}");
