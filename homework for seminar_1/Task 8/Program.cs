d3c43790a4b3015cfb7ed550a45767c4110b534d
ales <a.i11@bk.ru>
2022-11-27 03:28:29 +0300

    Initial commit

---
+++ homework for seminar_1/Task 8/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i <= a; i = i + 2)
+    Console.WriteLine(i);
