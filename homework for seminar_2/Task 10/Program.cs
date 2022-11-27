aab6e109ae4a6795fd12f60393d50e33c1f01375
ales <a.i11@bk.ru>
2022-11-28 02:07:51 +0300

    Added gitignore

--- homework for seminar_2/Task 10/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-Console.Clear();
-Console.WriteLine("Введите трёхзначное число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-int a2 = (a / 10) % 10;
-Console.WriteLine(a2);
