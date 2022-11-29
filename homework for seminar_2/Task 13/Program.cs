16b219ebd26dbffaf544e6b8e2e89719fb0f430c
ales <a.i11@bk.ru>
2022-11-30 01:49:02 +0300

    Added changes in Task 13

--- homework for seminar_2/Task 13/Program.cs
+++ homework for seminar_2/Task 13/Program.cs
@@ -3,11 +3,12 @@ Console.Clear();
 Console.Write("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 if (a > 99)
-    Console.Write((a / 10) % 10);
-    if (a > 9999)
-        Console.Write((a / 100) % 10);
+{
+    while (a > 999)
+    {
+        a = a / 10;
+    }
+        Console.Write(a % 10);
+}
 else
     Console.Write("Третьей цифры нет");
-    
-
-    
