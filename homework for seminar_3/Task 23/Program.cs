bf8bd8b24fa0729d60647be09efce212c299178e
ales <a.i11@bk.ru>
2022-12-04 02:06:52 +0300

    Added changes in Task 23

--- homework for seminar_3/Task 23/Program.cs
+++ homework for seminar_3/Task 23/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= a; i++)
+    Console.Write($"{i * i * i} ");
