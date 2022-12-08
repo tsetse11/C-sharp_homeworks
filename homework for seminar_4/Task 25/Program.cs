6cb7690ed4ab2186420437849256474e99ac1911
ales <a.i11@bk.ru>
2022-12-08 03:34:11 +0300

    Added changes in Task 25

---
+++ homework for seminar_4/Task 25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.Clear();
+int Factorial(int a, int b)
+{
+    int result = 1;
+    
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень, в которую необходимо возвести число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Factorial(num1, num2));
