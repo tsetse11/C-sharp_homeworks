72c70bf832aeed6915a414a8fc78cec1c1ddf33f
ales <a.i11@bk.ru>
2022-12-08 04:22:45 +0300

    Created Task 29

---
+++ homework for seminar_4/Task 29/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов, запрашивает данные(элементы массива) у пользователя и выводит их на экран.
+Console.Clear();
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+Console.Write("Введите элемент массива: ");
+array [i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
