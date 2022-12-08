7bc734df9c1e9a820f91771b6c659326cb0542b0
ales <a.i11@bk.ru>
2022-12-08 03:58:15 +0300

    Created Task 27 and added changes in Task 27

---
+++ homework for seminar_4/Task 27/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine()), count = 0;
+while (a != 0)
+{
+    count +=a % 10;
+    a /= 10;
+}
+Console.WriteLine(count);
