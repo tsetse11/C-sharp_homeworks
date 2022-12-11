469372c5f3c3a48ec1dd1085016ca831173aa898
ales <a.i11@bk.ru>
2022-12-11 09:14:08 +0300

    Added changes in Task 38

--- homework for seminar_5/Task 38/Program.cs
+++ homework for seminar_5/Task 38/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементами массива.
+void InputArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1, 51);
+}
+
+void DifferenceMaxMin(double[] array)
+{
+    double Max = array[0];
+    double Min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < Min)
+            Min = array[i];
+        if (array[i] > Max)
+            Max = array[i];
+    }
+    Console.Write($"Разность между максимальным и минимальным элементами массива: {Max - Min} ");
+}
+
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+DifferenceMaxMin(array);
