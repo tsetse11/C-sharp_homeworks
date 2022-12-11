6bd9907cb87b1fa9e937a2ef5dc902733ab674d6
ales <a.i11@bk.ru>
2022-12-11 06:29:56 +0300

    Added changes in Task 36

--- homework for seminar_5/Task 36/Program.cs
+++ homework for seminar_5/Task 36/Program.cs
@@ -1,2 +1,25 @@
 ﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
-Console.WriteLine("Hello, World!");
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1, 51);
+}
+
+void ReleaseArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 1)
+            sum += array[i];
+    }
+    Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях:{sum} ");
+}
+
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
