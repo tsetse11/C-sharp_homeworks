501d17db2f49f268de8f8de82b06f9ff3d2bfa9f
ales <a.i11@bk.ru>
2022-12-15 01:56:38 +0300

    Added changes in Task 47

--- homework for seminar_7/Task 47/Program.cs
+++ homework for seminar_7/Task 47/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами. m = 3, n = 4.
+
+void InputMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(new Random().NextDouble() * (10 - 0)+ 0, 2);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] coord = Console.ReadLine().Split(" ").Select(s => int.Parse(s)).ToArray();
+double[,] matrix = new double[coord[0], coord[1]];
+InputMatrix(matrix);
