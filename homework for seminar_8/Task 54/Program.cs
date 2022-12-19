0cc6471485b8eacd8179b00b781cec97839b702d
ales <a.i11@bk.ru>
2022-12-20 02:17:49 +0300

    Added changes in Task 54

--- homework for seminar_8/Task 54/Program.cs
+++ homework for seminar_8/Task 54/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {   
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(1, 11);
+            Console.Write($"{matrix[i,j]}\t");
+        }        
+        Console.WriteLine();
+    }
+}
+
+void Print(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {   
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} \t");                                      
+        }
+        Console.WriteLine();
+    }
+}
+
+void OrderElements(int[,] matrix)
+{    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {   
+        for (int k = 0; k < matrix.GetLength(1); k++)
+        {
+            int maxNumber = k;
+            for (int j = k + 1; j < matrix.GetLength(1); j++)
+            {   
+                if(matrix[i, j] > matrix[i, maxNumber])
+                maxNumber = j;
+            }
+            int temp = matrix[i, k];
+            matrix[i, k] = matrix[i, maxNumber];
+            matrix[i, maxNumber] = temp;
+        }
+
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] coord = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[coord[0], coord[1]];
+Console.WriteLine("Начальный массив: ");
+InputMatrix(matrix);
+OrderElements(matrix);
+Console.WriteLine("Конечный массив: ");
+Print(matrix);
